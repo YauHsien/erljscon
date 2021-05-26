715c8f1b97d084bc1b4e7e9d1fdf89a4c989442c
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2021-05-26 20:28:39 +0800

    自由輸入格式的 Parser ，包括 nibble/1 與 symbol/0

---
+++ parsec/src/formats.erl
@@ -0,0 +1,30 @@
+-module(formats).
+-include("include/types.hrl").
+-export(
+   [ nibble/1,
+     symbol/0
+   ]).
+
+-spec nibble(P::parser(char(),to())) -> parser(char(),to()).
+nibble(P) ->
+    combinators:xthen(white(), combinators:thenx(P,white())).
+
+white() ->
+    combinators:many(any(fun primitives:literal/1, " \t\n")).
+
+-spec any(PF :: fun((V::'case'()) -> parser(from(),to())),
+          List :: ['case'()]
+         ) ->
+          parser(from(), to()).
+any(P, List) ->
+    lists:foldl(
+      fun(X,Acc)-> combinators:alt(?REC(P(X)),Acc) end,
+      primitives:fail(),
+      List).
+
+conjunct(G, F) ->
+    fun(Inp) -> G(F(Inp)) end.
+
+-spec symbol() -> fun(([char()]) -> parser(char(),[char()])).
+symbol() ->
+    conjunct(fun nibble/1, applications:string()).
---
+++ parsec/test/formats_tests.erl
@@ -0,0 +1,20 @@
+-module(formats_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+nibble_test() ->
+    P = formats:nibble((applications:string())("hello")),
+    Inp = "\n\thello ,world",
+    ?assertMatch([{"hello",",world"},
+                  {"hello"," ,world"}
+                 ], P(Inp)),
+    ok.
+
+symbol_test() ->
+    P = (formats:symbol())("hi"),
+    Inp = "   hi   there",
+    ?assertMatch([{"hi","there"},
+                  {"hi"," there"},
+                  {"hi","  there"},
+                  {"hi","   there"}
+                 ], P(Inp)),
+    ok.
